9526de6e1b71c8a6ce2ecb3a0d0067443c2fbc32
Stanimir <v1ppers0nn@abv.bg>
2014-12-12 15:45:01 +0200

    Added footer

--- website/footer.php
+++ website/footer.php
@@ -2,11 +2,24 @@
     <footer>
         <section>
             <article>
-                <h3>Pages</h3>
-
+                <h2>Pages</h2>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Gallery</a></li>
+                        <li><a href="#">Other</a></li>
+                        <li><a href="#">About</a></li>
+                    </ul>
             </article>
             <article>Something</article>
-            <article>Something</article>
+            <article>
+                <h2>About</h2>
+                <ul>
+                    <li><a href="#">Nacho Tsvetkov </a></li>
+                    <li><a href="http://stanimirstoyanov.com/">Stanimir Stoyanov</a></li>
+                    <li><a href="#">Rumen Tsonkov </a></li>
+                    <li><a href="#">Daniel Georgiev</a></li>
+                </ul>
+            </article>
         </section>
     </footer>
     </div>
