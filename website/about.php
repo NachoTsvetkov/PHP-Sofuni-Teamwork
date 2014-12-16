e3ff14383a34207d3d3625b88fd37d98847e6c12
Daniel-Georgiev <daniko91@mail.bg>
2014-12-17 00:51:38 +0200

    forgot paths on about

--- website/about.php
+++ website/about.php
@@ -1,22 +1,22 @@
 <?php
-require 'common/header.php';
+require 'header.php';
 
 ?>
 
 <main>
     <?php
-     require 'common/aside.php'
+     require 'aside.php'
     ?>
     <div id="developersContainer">
     <h2>TEAM ATHENS</h2>
-    <a  target="_blank" href="https://softuni.bg/users/profile/show/nachotsvetkov" id="firstDeveloper" class="developers"><img src="../img/nacho.jpg"></a>
-    <a  target="_blank" href="http://stanimirstoyanov.com/" class="developers"><img src="../img/stanimir.jpg"></a>
-    <a  target="_blank" href="https://softuni.bg/users/profile/show/r.conkov" class="developers"><img src="../img/rumen.jpg"></a>
-    <a  target="_blank" href="https://softuni.bg/users/profile/show/macry" class="developers"><img src="../img/daniel.jpg"></a>
-    <a  target="_blank" href="https://github.com/NachoTsvetkov/PHP-Sofuni-Teamwork" id="gitHub"><img src="../img/dojocat.png"></a>
+    <a  target="_blank" href="https://softuni.bg/users/profile/show/nachotsvetkov" id="firstDeveloper" class="developers"><img src="img/nacho.jpg"></a>
+    <a  target="_blank" href="http://stanimirstoyanov.com/" class="developers"><img src="img/stanimir.jpg"></a>
+    <a  target="_blank" href="https://softuni.bg/users/profile/show/r.conkov" class="developers"><img src="img/rumen.jpg"></a>
+    <a  target="_blank" href="https://softuni.bg/users/profile/show/macry" class="developers"><img src="img/daniel.jpg"></a>
+    <a  target="_blank" href="https://github.com/NachoTsvetkov/PHP-Sofuni-Teamwork" id="gitHub"><img src="img/dojocat.png"></a>
     </div>
 </main>
 
 <?php
-require 'common/footer.php';
+require 'footer.php';
 ?>
