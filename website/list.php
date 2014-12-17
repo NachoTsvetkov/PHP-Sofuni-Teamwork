1d80336fdeb72aafdcc5e6ea8895b54deb00878c
Daniel-Georgiev <daniko91@mail.bg>
2014-12-17 23:37:20 +0200

    hopefully fixed

--- website/list.php
+++ website/list.php
@@ -6,7 +6,7 @@ $imageTags = $image -> get_random_image_tags(10, $db);
 
 foreach ($imageTags as $image) {
     echo "<article>
-            <a href=\"img/$image\" data-lightbox=\"image-1\" data-title=\"dadattata\"><img src=\"img/picture.jpg\" class='pictures'></a>
+            <a href='img/$image' data-lightbox='image-1' data-title='dadattata'><img src='img/$image' class='pictures'></a>
         </article>";
 }
 ?>
