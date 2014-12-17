b165bd5c3b7d481a75af0dffb8733b1fd6064835
Nacho Tsvetkov <nacho.tsvetkov@gmail.com>
2014-12-18 00:42:27 +0200

    ligthbox link upgrades

--- website/list.php
+++ website/list.php
@@ -7,8 +7,10 @@ $imageTags = $image -> get_random_image_tags(10, $db);
 foreach ($imageTags as $image) {
     $img = $image[0];
     $link = $image[1];
+    $download = $image[2];
+    $title = $image[3];
     echo "<article>
-            <a href='$link' data-lightbox='image-1' data-title='dadattata'>$img</a>
+            <a href='$link' data-lightbox='image-1' data-title='$title' download='$download'>$img</a>
         </article>";
 }
 ?>
