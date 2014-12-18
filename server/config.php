0d5682e99c38399110e4db336916f87fd202afc8
OmniPot <r.conkov@gmail.com>
2014-12-18 16:48:40 +0200

    config changes
    config changes

--- server/config.php
+++ server/config.php
@@ -9,49 +9,7 @@ if (session_status() == PHP_SESSION_NONE) {
     @session_start();
 }
 
-$_SESSION['isDev'] = true;
+$_SESSION['isDev'] = false;
 session_write_close();
-
- //$db = new DbConnection($_SESSION['isDev']);
- //$category = new Category();
- 
- //$result = $category -> get_category_images(1, $db);
- //var_dump($result);
- 
-// $image = new Image();
-
-
-//$result = $image -> get_image_comments (3, $db);
-//$result = $image -> comment (2, 6, 'Ebasi mackata!', $db);
-//var_dump($result);
-
-//$category = new Category();
-
-//$category -> add_category('best', $db);
-//$categories = $category -> get_categories_list($db);
-
-//foreach ($categories as $key => $value) {
-//    echo $value["category_name"] . "<br />";
-//}
-
-//var_dump($categories);
-
-// $user = new User();
-// $image = new Image();
-
-//echo $user -> check_user('neshto@gmail.com', $db);
-//echo $user -> add_user('Nacho', '123', 'nacho.tsvetkov@gmail.com', null, $db);
-//var_dump($user -> get_user('nacho.tsvetkov@gmail.com', '123', $db));
-
-//FETCH USER RESULTS
-//$user_result = $user -> get_user('Rumen', 'qkolud', $db);
-//var_dump($user_result);
-// $user_list_result = $user -> get_user_list($db);
-
-//FETCH IMAGE RESULTS
-// $image_result = $image -> get_image(1, $db);
-
-//IMAGE TAG CREATION;
-// echo '<img src="data:image/png/jpg/jpeg/gif;base64,'.base64_encode($image_result[0]).'" alt="photo" width="500px"><br>';
 
 ?>
