a733e092997f83ec3480f86933ff583670553546
OmniPot <r.conkov@gmail.com>
2014-12-18 11:10:54 +0200

    user changes
    user changes

--- website/user.php
+++ website/user.php
@@ -3,43 +3,57 @@
 require 'header.php';
 require 'aside.php';
 
+$db = new DbConnection($_SESSION['isDev']);
+$user = new User();
+$user  
+
+
 ?>
 <main>
     <script>
-        var array = document.getElementsByClassName('disabled');
-        var button = document.getElementById('editButton');
-        var uploadNewProfile = document.getElementById('disabled');
-        console.log(uploadNewProfile);
-        function hide(){
-            uploadNewProfile.style.display = "none";
-        }
-        button.disabled = true;
-        function enable() {
 
 
-                    for (var index in array) {
-                        array[index].disabled = false;
-                        console.log(array[index])
-                    }
-            uploadNewProfile.style.display="inline-block";
 
-                }
     </script>
- <figure id="profilePic">
-     <a href="#"><img width="200" height="200" src="img/random.jpg"></a>
- </figure>
-    <div id="userDetails" onload="hide()">
-    <form action="">
-        <label for="">Email:</label>
-        <input type="text" value="tupooooo@abv.bg" class="disabled" disabled/>
-        <label for="">Name:</label>
-        <input type="text" value="noname" class="disabled" disabled/>
-        <label class="disabled" id="disabled" >Upload new profile picture</label>
-        <input type="file" class="disabled"  disabled/>
-        <a href="#" class="btn btn-primary btn-xs" id="editButton" onclick="enable()">edit profile</a>
-        <input type="submit"  class="disabled" disabled />
-    </form>
-    </div>
+
+        <form action="">
+
+            <figure id="profilePic">
+                <a href="#"><img width="200" height="200" src="img/random.jpg"></a>
+            </figure>
+
+            <label for="">Email:</label>
+            <input type="text" value="tupooooo@abv.bg" disabled/>
+
+            <label for="">Name:</label>
+            <input type="text" value="noname" disabled/>
+
+            <label for="profilePic">Upload new profile picture</label>
+            <input id="profilePic" type="file" class="disabled"  disabled/>
+
+            <a href="#" class="btn btn-primary btn-xs" id="editButton">edit profile</a>
+            <input type="submit" disabled />
+        </form>
+
+        <form action="">
+
+            <figure id="profilePic">
+                <a href="#"><img width="200" height="200" src="img/random.jpg"></a>
+            </figure>
+
+            <label for="">Email:</label>
+            <input type="text" value="<?= echo $_SESSION['user_email'] ?>" disabled/>
+
+            <label for="">Name:</label>
+            <input type="text" value="<?= echo $_SESSION['user_name'] ?>" disabled/>
+
+            <label for="profilePic">Upload new profile picture</label>
+            <input id="profilePic" type="file" class="disabled"  disabled/>
+
+            <a href="#" class="btn btn-primary btn-xs" id="editButton">edit profile</a>
+            <input type="submit" disabled />
+        </form>
+
     <div id="albums">
 
     </div>
