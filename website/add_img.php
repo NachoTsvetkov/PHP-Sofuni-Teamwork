7ba4feb45ca7018201d3e792ae961bd42a160fa7
Nacho Tsvetkov <nacho.tsvetkov@gmail.com>
2014-12-18 05:18:28 +0200

    add_image general commit

--- website/add_img.php
+++ website/add_img.php
@@ -4,17 +4,26 @@ require '../server/config.php';
 require 'header.php';
 require 'aside.php';
 
+require_once 'google/appengine/api/cloud_storage/CloudStorageTools.php';
+use google\appengine\api\cloud_storage\CloudStorageTools;
+
+$options = [ 'gs_bucket_name' => 'my_bucket' ];
+$upload_url = CloudStorageTools::createUploadUrl('/upload_handler.php', $options);
+
 ?>
 
 <main>
      <form action="add_img" method="POST" enctype="multipart/form-data">
      	File:
-    	<input type="file" name ="image"/><input type="submit" value="Upload"/>
+    	<input type="file" name="image"/>
+        <input type="submit" value="Upload"/>
     </form>
  </main>
 
 <?php
 
+var_dump($_POST['image']);
 
+require 'footer.php';
 ?>
 
