14ac4f4f7679bfb88c405cd9e4f1f45181e5105b
Stanimir <v1ppers0nn@abv.bg>
2014-12-18 13:50:52 +0200

    footer

--- website/footer.php
+++ website/footer.php
@@ -1,11 +1,8 @@
     <footer class="navbar-default">
+           <h3>PICasso</h3>
+           <p><a href="about">About</a></p>
 
-
-                <h3>Pages</h3>
-
-
-                    <p><a href="about">About</a></p>
-                    <p>Something</p>
+            <iframe src="//www.facebook.com/plugins/like.php?href=http%3A%2F%2Fphp-teamwork-softuni.appspot.com%2F&amp;width&amp;layout=standard&amp;action=like&amp;show_faces=true&amp;share=true&amp;height=80&amp;appId=774976669189531" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:80px;" allowTransparency="true"></iframe>
 
     </footer>
     </div>
