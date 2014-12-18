fa5d4999fddbc8ad52a98173a79abe6cb1d92529
Daniel-Georgiev <daniko91@mail.bg>
2014-12-18 09:35:40 +0200

    add image uncomented

--- website/add_img.php
+++ website/add_img.php
@@ -7,22 +7,22 @@ require '../server/config.php';
 require 'header.php';
 require 'aside.php';
 
-//require_once 'google/appengine/api/cloud_storage/CloudStorageTools.php';
-//use google\appengine\api\cloud_storage\CloudStorageTools;
-//
-//$options = ['gs_bucket_name' => 'php-teamwork-softuni'];
-//
-//$upload_url = CloudStorageTools::createUploadUrl('/upload_handler', $options);
+require_once 'google/appengine/api/cloud_storage/CloudStorageTools.php';
+use google\appengine\api\cloud_storage\CloudStorageTools;
+
+$options = ['gs_bucket_name' => 'php-teamwork-softuni'];
+
+$upload_url = CloudStorageTools::createUploadUrl('/upload_handler', $options);
 
 ?>
 <main>
-<form action="" enctype="multipart/form-data"  method="post" id="imgUpload">
+<form action="<?php echo $upload_url?>" enctype="multipart/form-data"  method="post" id="imgUpload">
    <label for="impFile">Image to upload<input type="file" id="impFile" name="image_data" size="40"></label><br />
    <label for="txtImgTitle">Image Title<br/><input type="text" id="txtImgTitle" name="image_title" size="40"></label><br />
    <input type="submit" value="Upload" id="submitUpload" class="btn btn-primary">
 </form>
 </main>
-<?php //echo $upload_url?>
+
 <?php
 
 require 'footer.php';
