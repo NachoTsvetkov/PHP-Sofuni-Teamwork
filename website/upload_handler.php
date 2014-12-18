54f2fae57c1afc0bb8386459e840a2e7efa152e1
OmniPot <r.conkov@gmail.com>
2014-12-18 07:41:02 +0200

    nekvi raboti
    nekvi raboti

--- website/upload_handler.php
+++ website/upload_handler.php
@@ -1,10 +1,7 @@
 <?php
 
-
-var_dump($_FILES);
-
 $image = file_get_contents($_FILES['uploaded_files']['tmp_name']);
 
-echo '<img src="data:image/png/jpg/jpeg/gif;base64,'. base64_encode($image) .'" alt="photo" width="500px"><br>';
+// echo '<img src="data:image/png/jpg/jpeg/gif;base64,'. base64_encode($image) .'" alt="photo" width="500px"><br>';
 
 ?>
